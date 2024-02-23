e89516404dac8d2d7bdd0cb02d993deb82e1b174
alamincsme <alamincsme@gmail.com>
2024-02-23 19:13:16 +0600

    Update Logger info class

--- src/main/java/io/alamincsme/spring_aop/LoggerInfo.java
+++ src/main/java/io/alamincsme/spring_aop/LoggerInfo.java
@@ -1,9 +1,7 @@
 package io.alamincsme.spring_aop;
 
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,31 +15,53 @@ public class LoggerInfo {
 
    Logger logger = LoggerFactory.getLogger("LoggerInfo");
 
+//    @Pointcut ("execution( * io.alamincsme.spring_aop.Camera+.*(..))")
+//    public void cameraSnap(){
+//
+//    }
+
+//    @Pointcut("execution(* io.alamincsme.spring_aop.Lense+.*(..))")
+//    public void zoom () {
+//
+//    }
+
+
+//   @Before("cameraSnap() || zoom()")
+//    public void aboutToTakePhoto() {
+//        System.out.println("About to take photo ........");
+//    }
+
+//    @Pointcut("execution(* io.alamincsme.spring_aop.Camera+.*(..))")
+//    public void cameraActivity() {
+//
+//    }
+
+//    @Before("cameraActivity()")
+//    public void cameraRelatedActivity() {
+//        System.out.println("doing something related to camera ....");
+//    }
+
+
+ //another section
+
     @Pointcut ("execution( * io.alamincsme.spring_aop.Camera+.*(..))")
     public void cameraSnap(){
 
     }
 
-    @Pointcut("execution(* io.alamincsme.spring_aop.Lense+.*(..))")
-    public void zoom () {
 
-    }
-
-
-   @Before("cameraSnap() || zoom()")
+   @Before("cameraSnap() ")
     public void aboutToTakePhoto() {
         System.out.println("About to take photo ........");
     }
 
-    @Pointcut("execution(* io.alamincsme.spring_aop.Camera+.*(..))")
-    public void cameraActivity() {
-
+    @After("cameraSnap()")
+    public void afterAdvice () {
+     System.out.println("after advice .........");
     }
 
-    @Before("cameraActivity()")
-    public void cameraRelatedActivity() {
-        System.out.println("doing something related to camera ....");
+    @AfterReturning("cameraSnap()")
+    public void afterReturning() {
+        System.out.println("after returning advice ........");
     }
-
-
 }
