db37563a0a6f3cb5c77e24c31c5696e22aa984ae
alamincsme <alamincsme@gmail.com>
2024-02-23 14:26:57 +0600

    Spring  Aspect Oriented Programming learning

---
+++ src/main/java/io/alamincsme/springaoplearning/SpringAopLearningApplication.java
@@ -0,0 +1,13 @@
+package io.alamincsme.springaoplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopLearningApplication.class, args);
+	}
+
+}
