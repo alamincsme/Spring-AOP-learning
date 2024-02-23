93377501b0438728b3b3ad00f523a656373957b3
alamincsme <alamincsme@gmail.com>
2024-02-23 17:04:15 +0600

    Spring boot application class update

--- src/main/java/io/alamincsme/spring_aop/SpringAopLearningApplication.java
+++ src/main/java/io/alamincsme/spring_aop/SpringAopLearningApplication.java
@@ -1,14 +1,25 @@
-package io.alamincsme.springaoplearning;
+package io.alamincsme.spring_aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringAopLearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAopLearningApplication.class, args);
-		System.out.println("hello bangladesh.");
+
+		ApplicationContext context = SpringApplication.run(SpringAopLearningApplication.class, args);
+		Camera camera = context.getBean(Camera.class);
+		camera.snap();
+		camera.snap(1000);
+		camera.snap("Perliament House");
+		camera.snapNightTime();
+
+		Lense lense = context.getBean(Lense.class);
+		lense.zoom(10);
+
+		
 	}
 
 }
